381237b05ff906b2d20385eda7e5d189f381d9ae
CruptoJack <jackich.jack@mail.ru>
2025-09-30 04:40:46 +0700

    UPDATE: config mapper on struct

--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -17,12 +18,23 @@ type Config struct {
 	SSLmode  string
 }
 
-func (c Config) DNS() string {
+func ConfigFromENV() Config {
+	return Config{
+		User:     os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		Host:     os.Getenv("POSTGRES_HOST"),
+		Port:     os.Getenv("POSTGRES_PORT"),
+		DBName:   os.Getenv("POSTGRES_DB"),
+		SSLmode:  "disable",
+	}
+}
+
+func (c Config) DSN() string {
 	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", c.User, c.Password, c.Host, c.Port, c.DBName, c.SSLmode)
 }
 
 func NewPool(ctx context.Context, cnf Config) (*pgxpool.Pool, error) {
-	pcfg, err := pgxpool.ParseConfig(cnf.DNS())
+	pcfg, err := pgxpool.ParseConfig(cnf.DSN())
 	if err != nil {
 		return nil, err
 	}
